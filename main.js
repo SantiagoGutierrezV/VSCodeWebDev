bb348aec55f7c01d16d2642b20808f56ce7f6fec
Santiago <326sdg5@gmail.com>
2022-01-25 14:29:05 -0600

    Correcting js

--- main.js
+++ main.js
@@ -21,38 +21,38 @@ camera.position.setZ(30);
 renderer.render( scene, camera );
 
 
-const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
-const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } )
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } );
 const torus = new THREE.Mesh( geometry, material );
 
-scene.add(torus)
+scene.add(torus);
 
-const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(5, 5, 5)
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight)
+scene.add(pointLight, ambientLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50)
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
 // scene.add(lightHelper, gridHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25);
-  const material = new THREE.MeshStandardMaterial( { color: 0xffffff } )
+  const material = new THREE.MeshStandardMaterial( { color: 0xffffff } );
   const star = new THREE.Mesh( geometry, material );
 
   const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ) );
   star.position.set(x, y, z);
-  scene.add(star)
-}
+  scene.add(star);
+};
 
-Array(200).fill().forEach(addStar)
+Array(200).fill().forEach(addStar);
 
 const spaceTexture = new THREE.TextureLoader().load('space.jpg');
-scene.background = spaceTexture
+scene.background = spaceTexture;
 
 const santiTexture = new THREE.TextureLoader().load('ProfessionalPictureSquare.jpg');
 
@@ -65,7 +65,7 @@ scene.add(santi);
 
 // Moon
 
-const moonTexture = new THREE.TextureLoader().load('moon.jpg')
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
 const normalTexture = new THREE.TextureLoader().load('normal.jpg');
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
